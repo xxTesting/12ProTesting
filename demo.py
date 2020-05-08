6fbcc17e8902dd8664b8c43430f60bb36f38430a
sydj_cdw <cdw2013520163>
2020-05-08 11:53:45 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+# from python.demo1 import Demo1
+# from testing import demo2
+# from testing.demo2 import Demo2
+from testing.demo2 import *
+# Demo1()
+# Demo2()
+
+print(hello)
+f()
---
+++ testing/demo2.py
@@ -0,0 +1,13 @@
+
+# __all__=['hello']
+
+# hello = 'hello demo2'
+#
+# def f():
+#     print('demo2.py f()')
+#
+# class Demo2:
+#     pass
+
+from testing import *
+demo2
